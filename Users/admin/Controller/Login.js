687f0e5720b1d173a6fd9b77d5b9ca9092a812c8
sachinrao2811 <sachinrao9450@gmail.com>
2023-02-08 15:10:07 +0530

    implement in login validation

--- Users/admin/Controller/Login.js
+++ Users/admin/Controller/Login.js
@@ -2,11 +2,14 @@ const loginSchema = require("../Modals/LoginSchema");
 
 const login = async (req, resp) => {
   const admin = await loginSchema.findOne(req.body);
-  console.log("called function");
-  if (admin) {
-    resp.send(admin);
+  if (!req.body.email && req.body.password) {
+    if (admin) {
+      resp.send(admin);
+    } else {
+      resp.send({ response: "no user available" });
+    }
   } else {
-    resp.send({ response: "no user available" });
+    resp.send(" please enter email and password");
   }
 };
 
