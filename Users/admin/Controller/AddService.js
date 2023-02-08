3adaa1627c8adbbad07bf78699268f5fa4105b49
sachinrao2811 <sachinrao9450@gmail.com>
2023-02-08 16:07:59 +0530

    implement in add service form

--- Users/admin/Controller/AddService.js
+++ Users/admin/Controller/AddService.js
@@ -1,9 +1,18 @@
 const ServiceSch = require("../Modals/ServiceSchema");
 
 const AddService = async (req, resp) => {
-  const data = new ServiceSch(req.body);
-  await data.save();
-  resp.send("service successfully added");
+  const FilterUser = ServiceSch.findOne({
+    name: req.body.name,
+    email: req.body.email,
+    description: req.body.description,
+  });
+  if (!FilterUser) {
+    const data = new ServiceSch(req.body);
+    await data.save();
+    resp.send("service successfully added");
+  } else {
+    resp.send("This Service is already available");
+  }
 };
 
 module.exports = AddService;
--- Users/admin/Controller/SignUp.js
+++ Users/admin/Controller/SignUp.js
@@ -1,9 +1,14 @@
 const AdminRegister = require("../Modals/LoginSchema");
 
 const register = async (req, resp) => {
-  const data = new AdminRegister(req.body);
-  await data.save();
-  resp.send("register successfully");
+  const FilterData = await AdminRegister.findOne({ email: req.body.email });
+  if (!FilterData) {
+    const data = new AdminRegister(req.body);
+    await data.save();
+    resp.send("register successfully");
+  } else {
+    resp.send(" already available this user");
+  }
 };
 
 module.exports = register;
