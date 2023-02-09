d2e733fcaf464dbf4894b21bfcd346ccba18864d
sachinrao2811 <sachinrao9450@gmail.com>
2023-02-09 15:10:17 +0530

    login validaation

--- Users/admin/Controller/Login.js
+++ Users/admin/Controller/Login.js
@@ -2,14 +2,10 @@ const loginSchema = require("../Modals/LoginSchema");
 
 const login = async (req, resp) => {
   const admin = await loginSchema.findOne(req.body);
-  if (!req.body.email && req.body.password) {
-    if (admin) {
-      resp.send(admin);
-    } else {
-      resp.send({ response: "no user available" });
-    }
+  if (admin) {
+    resp.send(admin);
   } else {
-    resp.send(" please enter email and password");
+    resp.send({ response: "Not Found" });
   }
 };
 
